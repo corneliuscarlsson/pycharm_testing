6196c1065285653512d536b4f9cf5f1a5b2e1311
corneliuscarlsson <corneliuscarlsson@yahoo.com>
2022-01-31 22:16:04 +0100

    First ever commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# if _a=None_ == '__main__':
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# TODO: Fix Bug
+# TODO: Implement Tests
+print("Pycharm is Awesome")
+x = np.arange(0, 4 * np.pi, 0.1)
+y = np.sin(x)
+
+# plt.plot(x, y)
+# plt.show()
+
+def add(a, b):
+    c = a + b
+    return c
+
+
+print(add(2, 3))
